9d523c9bcf5dd1062d02daa5982842244c403f07
khiem <khiem@yahoo.com>
2016-09-23 17:02:30 +0700

    update

--- Angular2-express-mongoose-gulp-node-typescript-Sample/server/src/config/constants/constants.ts
+++ Angular2-express-mongoose-gulp-node-typescript-Sample/server/src/config/constants/constants.ts
@@ -3,7 +3,7 @@
  */
 
 class Constants {
-    static DB_CONNECTION_STRING: string = process.env.NODE_ENV === 'production' ? process.env.dbURI : "mongodb://localhost:27017/quickStart"
+    static DB_CONNECTION_STRING: string = process.env.NODE_ENV === 'production' ? process.env.dbURI : "mongodb://root:123456@ds035786.mlab.com:35786/angular_expressjs"
 }
 Object.seal(Constants);
 export = Constants;

5c01f5440058e7077cda5eef831308bf8f4da2f3
khiem <khiem@yahoo.com>
2016-09-28 17:25:56 +0700

    update

--- Angular2_Nodejs_Gulp/client/app/app.routing.ts
+++ Angular2_Nodejs_Gulp/client/app/app.routing.ts
@@ -3,9 +3,12 @@ import { NgModule , ModuleWithProviders} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component/home.component';
+import { CreateUserComponent } from './createUserComponent/createUser.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'create-user', component: CreateUserComponent },
 ];
 
 export var routing:ModuleWithProviders = RouterModule.forRoot(routes)
--- Angular2_Nodejs_Gulp/dist/app/app.routing.js
+++ Angular2_Nodejs_Gulp/dist/app/app.routing.js
@@ -1,8 +1,10 @@
 "use strict";
 var router_1 = require('@angular/router');
 var home_component_1 = require('./home.component/home.component');
+var createUser_component_1 = require('./createUserComponent/createUser.component');
 var routes = [
     { path: '', component: home_component_1.HomeComponent },
+    { path: 'create-user', component: createUser_component_1.CreateUserComponent },
 ];
 exports.routing = router_1.RouterModule.forRoot(routes);
 

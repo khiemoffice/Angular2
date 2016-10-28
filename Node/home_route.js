3c62b1725704784ae7c02ba0ad53f76c16dd6900
khiem <khiem@yahoo.com>
2016-10-28 17:23:47 +0700

    update

--- Node/home_route.js
+++ Node/home_route.js
@@ -1,13 +1,26 @@
 var express = require('express')
   , router = express.Router();
 
+var User     = require('./models/user');
+
 
 // Car brands page
 router.get('/', function(req, res) {
   res.sendfile(__dirname+'/index.html');
 })
 router.post('/', function(req, res) {
-  res.end(req.body.username);
+    //res.end(req.body.username);
+    var newUser = new User();
+    var username = req.body.username;
+    var password = req.body.password;
+
+     newUser.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'User created!' });
+        });
+    
 })
 // Car brands page
 router.get('/brands', function(req, res) {
--- Node/server.js
+++ Node/server.js
@@ -4,8 +4,15 @@ var port=process.env.PORT || 3000;
 var app= express();
 var cars=  require('./home_route');
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var User     = require('./models/user');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+mongoose.connect('mongodb://root:123456@ds029814.mlab.com:29814/admindata');
 
 
 app.use('/home', cars);
